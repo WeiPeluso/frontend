0c41d8cb03032095ccacef24478949f7cb339cad
Collin Ballou <BlueEagle@users.noreply.github.com>
2020-06-24 10:03:04 -0600

    styling added to nav and links

--- src/App.js
+++ src/App.js
@@ -7,16 +7,18 @@ import Dashboard from "./components/Dashboard";
 import PrivateRoute from "./utils/PrivateRoute";
 import AddStudent from "./components/AddStudent";
 import StudentPage from "./components/StudentPage";
+import styled from "styled-components"
+
 function App() {
   return (
     <Router>
       <div className="App">
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/login">Login</Link>
-          <Link to="/user">Dashboard</Link>
-          <Link to="/register">Register Now</Link>
-        </nav>
+        <StyledNav>
+          <StyledLink to="/">Home</StyledLink>
+          <StyledLink to="/login">Login</StyledLink>
+          <StyledLink to="/user">Dashboard</StyledLink>
+          <StyledLink to="/register">Register Now</StyledLink>
+        </StyledNav>
       </div>
 
       <Route exact path="/login" component={LoginForm} />
@@ -33,3 +35,26 @@ function App() {
 }
 
 export default App;
+
+const StyledNav = styled.nav`
+  height: 7vh;
+  background-color: #0A2738;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  box-shadow: 0 0 3px black;
+`
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: white;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &:hover {
+    background-color: #A1A7AA;
+    color: black;
+  }
+`
