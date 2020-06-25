74f08253b86977cd6958ac1673d451777bcf775b
Wei Peluso <weipeluso@MacBook-Pro.local>
2020-06-24 22:23:02 -0400

    added collin ballou's style

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,6 +3,7 @@ import { Link, useParams } from "react-router-dom";
 import StudentsList from "./StudentsList";
 import { axiosWithAuth } from "../utils/axiosWithAuth";
 import { useSelector } from "react-redux";
+import styled from "styled-components";
 
 const Dashboard = (props) => {
   const userID = useSelector((state) => state.userReducer.id);
@@ -21,19 +22,49 @@ const Dashboard = (props) => {
     }
   }, [refresh]);
   return (
-    <>
-      <div className="reminder">
-        This is the reminder section , comming up soon
-      </div>
+    <DashboardContainer>
+      <ReminderDiv className="reminder">
+        This is the reminder section, coming soon!
+      </ReminderDiv>
 
-      <div className="students">
-        <Link to="/addstudent">
-          <button>Add a Student</button>
-        </Link>
+      <StudentsListDiv className="students">
+        <ButtonLink to="/addstudent">Add a Student</ButtonLink>
         <StudentsList students={students} />
-      </div>
-    </>
+      </StudentsListDiv>
+    </DashboardContainer>
   );
 };
 
 export default Dashboard;
+
+const DashboardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+const ReminderDiv = styled.div`
+  background-color: lightgoldenrodyellow;
+  border-radius: 5px;
+  padding: 3%;
+  width: 90%;
+  margin: 2%;
+  box-shadow: 0 0 3px black;
+`;
+const ButtonLink = styled(Link)`
+  text-decoration: none;
+  color: white;
+  background-color: #0a2738;
+  padding: 0.5rem 1.5rem;
+  border-radius: 20px;
+  box-shadow: 1px 1px 2px black;
+  &:hover {
+    background-color: #a1a7aa;
+    color: black;
+  }
+`;
+const StudentsListDiv = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
--- src/components/StudentsList.js
+++ src/components/StudentsList.js
@@ -1,24 +1,61 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import StudentCard from "./StudentCard";
+import styled from "styled-components";
 
 const StudentList = (props) => {
   return (
-    <>
+    <StudentListContainer>
       {props.students.map((student, index) => {
         return (
           <div key={index}>
-            <Link
-              to={`/student/${student.id}`}
+            <StudentContainerLink
+              to={{
+                pathname: `/student/${student.id}`,
+                state: student,
+              }}
               style={{ textDecoration: "none" }}
             >
-              <StudentCard student={student} />
-            </Link>
+              <p>
+                <LabelSpan>Name: </LabelSpan>
+                {student.name}
+              </p>
+              <p>
+                <LabelSpan>Email: </LabelSpan>
+                {student.email}
+              </p>
+              <p>
+                <LabelSpan>Subject: </LabelSpan>
+                {student.subject}
+              </p>
+            </StudentContainerLink>
           </div>
         );
       })}
-    </>
+    </StudentListContainer>
   );
 };
 
 export default StudentList;
+
+const StudentListContainer = styled.div`
+  box-sizing: border-box;
+  margin-top: 2%;
+  width: 97%;
+  box-shadow: 0 0 3px black;
+  border-radius: 5px;
+  padding: 1%;
+  background-color: lightgray;
+`;
+const StudentContainerLink = styled(Link)`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: 0 0 2px black;
+  color: black;
+`;
+const LabelSpan = styled.span`
+  color: gray;
+`;
